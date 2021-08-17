84c403fb8ee3503903f0a9de574bc309ebf818d4
Andre <katsuya.tsu@gmail.com>
2021-08-17 16:42:43 -0300

    added assets and effects, added launch, createPlayer and deg2rad methods

--- Project29/GameScene.swift
+++ Project29/GameScene.swift
@@ -14,12 +14,18 @@ enum CollisionTypes: UInt32 {
     case player = 4
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     //MARK: - Attributes
     
     var buildings = [BuildingNode]()
     weak var viewController: GameViewController?
+    
+    var player1: SKSpriteNode!
+    var player2: SKSpriteNode!
+    var banana: SKSpriteNode!
+
+    var currentPlayer = 1
     
     //MARK: - didMove
     
@@ -27,7 +33,10 @@ class GameScene: SKScene {
         
         backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
 
-            createBuildings()
+        createBuildings()
+        createPlayers()
+        
+        physicsWorld.contactDelegate = self
     }
     
     //MARK: - Methods
@@ -49,7 +58,121 @@ class GameScene: SKScene {
             }
     }
     
+    func deg2rad(degrees: Int) -> Double {
+        return Double(degrees) * Double.pi / 180
+    }
+    
     func launch(angle: Int, velocity: Int) {
+        // how hard to throw the banana.
+           let speed = Double(velocity) / 10.0
+
+           // Convert the input angle to radians.
+           let radians = deg2rad(degrees: angle)
+
+           // If somehow there's a banana already, we'll remove it then create a new one using circle physics.
+           if banana != nil {
+               banana.removeFromParent()
+               banana = nil
+           }
+
+           banana = SKSpriteNode(imageNamed: "banana")
+           banana.name = "banana"
+           banana.physicsBody = SKPhysicsBody(circleOfRadius: banana.size.width / 2)
+           banana.physicsBody?.categoryBitMask = CollisionTypes.banana.rawValue
+           banana.physicsBody?.collisionBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+           banana.physicsBody?.contactTestBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+           banana.physicsBody?.usesPreciseCollisionDetection = true
+           addChild(banana)
+
+           if currentPlayer == 1 {
+               // If player 1 was throwing the banana, we position it up and to the left of the player and give it some spin.
+               banana.position = CGPoint(x: player1.position.x - 30, y: player1.position.y + 40)
+               banana.physicsBody?.angularVelocity = -20
+
+               // Animate player 1 throwing their arm up then putting it down again.
+               let raiseArm = SKAction.setTexture(SKTexture(imageNamed: "player1Throw"))
+               let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+               let pause = SKAction.wait(forDuration: 0.15)
+               let sequence = SKAction.sequence([raiseArm, pause, lowerArm])
+               player1.run(sequence)
+
+               // Make the banana move in the correct direction.
+               let impulse = CGVector(dx: cos(radians) * speed, dy: sin(radians) * speed)
+               banana.physicsBody?.applyImpulse(impulse)
+           } else {
+               // If player 2 was throwing the banana, we position it up and to the right, apply the opposite spin, then make it move in the correct direction.
+               banana.position = CGPoint(x: player2.position.x + 30, y: player2.position.y + 40)
+               banana.physicsBody?.angularVelocity = 20
+
+               let raiseArm = SKAction.setTexture(SKTexture(imageNamed: "player2Throw"))
+               let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+               let pause = SKAction.wait(forDuration: 0.15)
+               let sequence = SKAction.sequence([raiseArm, pause, lowerArm])
+               player2.run(sequence)
+
+               let impulse = CGVector(dx: cos(radians) * -speed, dy: sin(radians) * speed)
+               banana.physicsBody?.applyImpulse(impulse)
+           }
+    }
+    
+    func createPlayers() {
         
+        //Create a player sprite and name it "player1".
+        player1 = SKSpriteNode(imageNamed: "player")
+        player1.name = "player1"
+        player1.physicsBody = SKPhysicsBody(circleOfRadius: player1.size.width / 2)
+        player1.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        //Create a physics body for the player that collides with bananas, and set it to not be dynamic.
+        player1.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.isDynamic = false
+
+        let player1Building = buildings[1]
+        //Position the player at the top of the second building in the array. (This is why we needed to keep an array of the buildings.)
+        player1.position = CGPoint(x: player1Building.position.x, y: player1Building.position.y + ((player1Building.size.height + player1.size.height) / 2))
+        //Add the player to the scene.
+        addChild(player1)
+
+        //repeat but position player2 in the second to last building
+        player2 = SKSpriteNode(imageNamed: "player")
+        player2.name = "player2"
+        player2.physicsBody = SKPhysicsBody(circleOfRadius: player2.size.width / 2)
+        player2.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player2.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.isDynamic = false
+
+        let player2Building = buildings[buildings.count - 2]
+        player2.position = CGPoint(x: player2Building.position.x, y: player2Building.position.y + ((player2Building.size.height + player2.size.height) / 2))
+        addChild(player2)
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        let firstBody: SKPhysicsBody
+            let secondBody: SKPhysicsBody
+
+            if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+                firstBody = contact.bodyA
+                secondBody = contact.bodyB
+            } else {
+                firstBody = contact.bodyB
+                secondBody = contact.bodyA
+            }
+
+            guard let firstNode = firstBody.node else { return }
+            guard let secondNode = secondBody.node else { return }
+
+            if firstNode.name == "banana" && secondNode.name == "building" {
+                bananaHit(building: secondNode, atPoint: contact.contactPoint)
+            }
+
+            if firstNode.name == "banana" && secondNode.name == "player1" {
+                destroy(player: player1)
+            }
+
+            if firstNode.name == "banana" && secondNode.name == "player2" {
+                destroy(player: player2)
+            }
     }
 }
