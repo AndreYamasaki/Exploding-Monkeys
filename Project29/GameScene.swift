07f2a2b8a881651d5539445af907ab03a79f5c17
Andre <katsuya.tsu@gmail.com>
2021-08-17 12:01:56 -0300

    Initial Commit

---
+++ Project29/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by user on 17/08/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+}
